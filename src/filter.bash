db5018c22972b15258ee44d4518a00f689b6aba7
vanstrouble <vanstrouble@proton.me>
2025-03-24 16:27:50 -0600

    Enhance filter script to support timed sessions for Amphetamine

--- src/filter.bash
+++ src/filter.bash
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+INPUT="$1"
 STATE=$(osascript -e 'tell application "Amphetamine" to return session is active' 2>/dev/null)
 
-if [[ "$STATE" == "true" ]]; then
-  echo '{"items":[{"title":"Turn Off","subtitle":"Allow computer to sleep","arg":"off","icon":{"path":"icon.png"}}]}'
+if [[ "$INPUT" =~ ^[0-9]+$ ]]; then
+  END_TIME=$(date -v+"$INPUT"M +"%H:%M")
+
+  echo '{"items":[{"title":"Turn On for '"$INPUT"' minutes","subtitle":"Keep awake until around '"$END_TIME"'","arg":"'"$INPUT"'","icon":{"path":"icon.png"}}]}'
 else
-  echo '{"items":[{"title":"Turn On","subtitle":"Prevent sleep indefinitely","arg":"on","icon":{"path":"icon.png"}}]}'
+  if [[ "$STATE" == "true" ]]; then
+    echo '{"items":[{"title":"Turn Off","subtitle":"Allow computer to sleep","arg":"off","icon":{"path":"icon.png"}}]}'
+  else
+    echo '{"items":[{"title":"Turn On","subtitle":"Prevent sleep indefinitely","arg":"on","icon":{"path":"icon.png"}}]}'
+  fi
 fi
