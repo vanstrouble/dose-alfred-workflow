0f8d78b0a8b509e53b550f4e823ebdbe8a9332a5
vanstrouble <vanstrouble@proton.me>
2025-03-25 14:38:05 -0600

    Add notification feature to ams_enhancement script for Amphetamine control

--- src/ams_enhancement.zsh
+++ src/ams_enhancement.zsh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+function notification {
+    ./notificator --title "Amphetamine Control" --message "${1}"
+}
+
 INPUT="$1"
 
+if [[ -n "$hotkey_value" ]]; then
+    INPUT="$hotkey_value"
+fi
+
+# Depuración
+echo "DEBUG: INPUT='$INPUT', HOTKEY_VALUE='$hotkey_value'" >&2
+
 if [[ "$INPUT" =~ ^[0-9]+$ ]]; then
-  osascript -e "tell application \"Amphetamine\" to start new session with options {duration:$INPUT, interval:minutes, displaySleepAllowed:false}"
+    osascript -e "tell application \"Amphetamine\" to start new session with options {duration:$INPUT, interval:minutes, displaySleepAllowed:false}"
+    notification "Amphetamine activado por $INPUT minutos."
 elif [[ "$INPUT" == "on" ]]; then
-  osascript -e "tell application \"Amphetamine\" to start new session"
+    osascript -e "tell application \"Amphetamine\" to start new session"
+    notification "Amphetamine activado."
 elif [[ "$INPUT" == "off" ]]; then
-  osascript -e "tell application \"Amphetamine\" to end session"
+    osascript -e "tell application \"Amphetamine\" to end session"
+    notification "Amphetamine desactivado."
 else
-  echo "Usage: $0 [on|off|minutes]"
+    echo "Usage: $0 [on|off|minutes]"
 fi
